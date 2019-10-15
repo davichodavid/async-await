58491a5bc99092316fd0bbdfe3704fc513a6e270
davichodavid <davichodavid@yahoo.com>
2019-10-15 12:36:53 -0400

    is rendering data using hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const fetchedData = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      setData(fetchedData.data);
+      console.log(fetchedData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='App'>
+      {data.map(clump => (
+        <p key={clump.id}>
+          {clump.title} is the reason I like the number {clump.id}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
+      will this work
     </div>
   );
 }
